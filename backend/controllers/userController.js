7be00c56e94a7316f7fd6212bc3e39d189638f24
Aman Shrivastava <shrivastavaaman1601@gmail.com>
2026-07-16 16:54:21 +0530

    Add admin API to fetch user by ID

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -77,9 +77,34 @@ const deleteUser = async (req, res) => {
     });
   }
 };
+// Get Single User
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
-  getAllUsers,
-  updateUser,
-  deleteUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+    deleteUser
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,9 +5,10 @@ const protect = require("../middleware/authMiddleware");
 const adminOnly = require("../middleware/adminMiddleware");
 
 const {
-  getAllUsers,
-  updateUser,
-  deleteUser,
+    getAllUsers,
+    getUserById,
+    updateUser,
+    deleteUser
 } = require("../controllers/userController");
 
 router.get("/", protect, adminOnly, getAllUsers);
@@ -15,5 +16,7 @@ router.get("/", protect, adminOnly, getAllUsers);
 router.put("/:id", protect, adminOnly, updateUser);
 
 router.delete("/:id", protect, adminOnly, deleteUser);
+
+router.get("/:id", protect, adminOnly, getUserById);
 
 module.exports = router;
