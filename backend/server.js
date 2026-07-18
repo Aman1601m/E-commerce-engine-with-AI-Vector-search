26e186f6b993c565141a5568375d1af43a6ff2d2
shiikharrr <shikhargupta.137@gmail.com>
2026-07-18 12:41:07 +0530

    feat(database): configure mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -2,29 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./config/db.js";
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
-
 // Middleware
 app.use(cors());
-app.use(express.json());    
+app.use(express.json());
 
-
-// Routes
+// Health Check Route
 app.get("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "High Performance E-commerce Engine is running ",
-    });
+  res.status(200).json({
+    success: true,
+    message: "High Performance E-commerce Engine is running",
+  });
 });
 
-
-// Start the server
 const PORT = process.env.PORT || 5000;
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
