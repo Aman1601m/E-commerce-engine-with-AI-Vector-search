2172c36914bddc506c0adfaec5d93317762eb9b3
shiikharrr <shikhargupta.137@gmail.com>
2026-07-18 13:08:34 +0530

    feat(product): implement product routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+import {
+  createProductController,
+  getAllProductsController,
+  getProductByIdController,
+  updateProductController,
+  deleteProductController,
+} from "../controllers/productController.js";
+
+const router = express.Router();
+
+// Create Product
+router.post("/", createProductController);
+
+// Get All Products
+router.get("/", getAllProductsController);
+
+// Get Product By ID
+router.get("/:id", getProductByIdController);
+
+// Update Product
+router.put("/:id", updateProductController);
+
+// Delete Product
+router.delete("/:id", deleteProductController);
+
+export default router;
