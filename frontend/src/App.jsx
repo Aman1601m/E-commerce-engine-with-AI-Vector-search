9d62fc623eb3e1e2bd69a73e863c4e801c86b6c0
Santhosh <santhosh0616r@gmail.com>
2026-07-21 19:07:38 +0530

    refactor: improve dashboard layout with sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
+
       <Navbar />
-      <AppRoutes />
+
+      <div className="flex">
+        <Sidebar />
+
+        <div className="flex-1">
+          <AppRoutes />
+        </div>
+      </div>
+
     </div>
   );
 }
